c67a68601d69df7a3056725e600cb861b4eb54c0
Vaden Masrani <vadmas@cs.ubc.ca>
2019-07-04 19:25:46 -0700

    Major changes to jobsubmitter, added default notebooks

--- .python/db.py
+++ .python/db.py
@@ -18,14 +18,20 @@ DATABASE_SERVER   = 'headnode'
 
 REMOTE_SERVER = 'remote.cs.ubc.ca'
 REMOTE_SERVER_USERNAME = 'vadmas'
-REMOTE_SERVER_PASSWORD = None # Assumes you have password-free access via public keys
-REMOTE_MONGO_URI = "mongodb://{}:{}@{}:{}/{}".format(DATABASE_USERNAME, DATABASE_PASSWORD, REMOTE_SERVER, DATABASE_PORT, DATABASE_NAME)
+REMOTE_SERVER_PASSWORD = None  # Assumes you have password-free access via public keys
+REMOTE_MONGO_URI = "mongodb://{}:{}@{}:{}/{}".format(DATABASE_USERNAME,
+                                                     DATABASE_PASSWORD,
+                                                     REMOTE_SERVER,
+                                                     DATABASE_PORT,
+                                                     DATABASE_NAME)
 SSH_CONTROL_PATH = Path('./.ssh_control_path')
 SSH_SESSION = "%r@%h:%p_{}".format(uuid.uuid1())
 
-def init():
-    ex = Experiment()
-    ex = add_source_file(ex) #To avoid db.py being source file
+
+def init(ex=None):
+    if ex is None:
+        ex = Experiment()
+        ex = add_source_file(ex)  # To avoid db.py being source file
     if '--unobserved' in sys.argv:
         return ex
     if test_connection(REMOTE_MONGO_URI):
@@ -47,27 +53,32 @@ def test_connection(uri, timeout=5):
         print(ex)
         raise
 
+
 def open_ssh():
     open_port = find_open_port()
     print("Opening ssh tunnel on port:", open_port)
-    SSH_CONTROL_PATH.mkdir(parents=True,  exist_ok=True)
+    SSH_CONTROL_PATH.mkdir(parents=True, exist_ok=True)
     # os.system('ssh -f -N -M -S {}/{} -L {}:headnode:27017 {}@{}'.format(SSH_CONTROL_PATH, SSH_SESSION, open_port, REMOTE_SERVER_USERNAME, REMOTE_SERVER))
-    os.system('ssh -f -N -M -S {}/{} -L {}:{}:27017 {}@{}'.format(SSH_CONTROL_PATH, SSH_SESSION, open_port, DATABASE_SERVER, REMOTE_SERVER_USERNAME, REMOTE_SERVER))
+    os.system('ssh -f -N -M -S {}/{} -L {}:{}:27017 {}@{}'.format(SSH_CONTROL_PATH,
+                                                                  SSH_SESSION, open_port, DATABASE_SERVER, REMOTE_SERVER_USERNAME, REMOTE_SERVER))
     atexit.register(close_ssh)
     ssh_mongo_uri = REMOTE_MONGO_URI.replace(REMOTE_SERVER, "localhost").replace(DATABASE_PORT, open_port)
-    sleep(1) # Give the tunnel a second to connect
+    sleep(1)  # Give the tunnel a second to connect
     assert test_connection(ssh_mongo_uri), "Error, SSH connection not established"
     return ssh_mongo_uri
+
 
 def add_source_file(ex):
     filename = Path(os.getcwd()) / sys.argv[0]
     ex.add_source_file(filename)
     return ex
 
+
 def find_open_port():
     s = socket.socket()
     s.bind(('', 0))                 # Bind to a free port provided by the host.
     return str(s.getsockname()[1])  # Return the port number assigned.
+
 
 def close_ssh():
     os.system('ssh -S {}/{} -O exit {}@{}'.format(SSH_CONTROL_PATH, SSH_SESSION, REMOTE_SERVER_USERNAME, REMOTE_SERVER))
