cb0f0c7f8d96ca4e194a72039e7250e240ef3164
Vaden Masrani <vadmas@gmail.com>
2024-02-10 22:42:24 -0500

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+
+ln -sf $HOME/dotfiles/.aliases.zsh $HOME/.aliases.zsh
+ln -sf $HOME/dotfiles/.bash_logout $HOME/.bash_logout
+ln -sf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
+ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/.fzf-config.zsh $HOME/.fzf-config.zsh
+ln -sf $HOME/dotfiles/.fzf.bash $HOME/.fzf.bash
+ln -sf $HOME/dotfiles/.fzf.zsh $HOME/.fzf.zsh
+ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+ln -sf $HOME/dotfiles/.pdbhistory $HOME/.pdbhistory
+ln -sf $HOME/dotfiles/.profile $HOME/.profile
+ln -sf $HOME/dotfiles/.pylintrc $HOME/.pylintrc
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/.zlogin $HOME/.zlogin
+ln -sf $HOME/dotfiles/.zlogout $HOME/.zlogout
+ln -sf $HOME/dotfiles/.zpreztorc $HOME/.zpreztorc
+ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
+ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+
+
+mkdir $HOME/bin
+chmod -x $HOME/bin/bat_exa_preview.sh
+chmod -x $HOME/bin/rfz.sh
+ln -sf $HOME/dotfiles/bat_exa_preview.sh $HOME/bin/bat_exa_preview
+ln -sf $HOME/dotfiles/rfz.sh $HOME/bin/rfz
+
+
+
+
