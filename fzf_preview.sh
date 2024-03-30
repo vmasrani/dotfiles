b9bd3eb5117428f5e0c4f44d6a98c6855b528d5c
Vaden Masrani <vadmas@gmail.com>
2024-03-30 00:06:43 -0700

    img and pdf preview

--- fzf_preview.sh
+++ fzf_preview.sh
@@ -9,6 +9,10 @@ if [[ -f $1 ]]; then
     pq '' "$1"
   elif [[ $1 == *.pt ]]; then
     torch-preview "$1"
+  elif [[ $1 == *.jpg || $1 == *.jpeg || $1 == *.png || $1 == *.gif ]]; then
+    chafa --size=80x80 "$1"
+  elif [[ $1 == *.pdf ]]; then
+    pdftotext "$1" -
   else
     if command -v "$1" &> /dev/null; then
       man "$1"
