cbdcce847bc7760870810df221b80f3fd909a118
Vaden Masrani <vadmas@gmail.com>
2024-04-26 12:31:15 -0700

    added npm and cargo

--- install_npm.sh
+++ install_npm.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 NODE_DIR="$HOME/.local/lib/nodejs"
 
-VERSION=$(curl -s https://nodejs.org/dist/latest/ | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*' | head -n 1)
-
+VERSION=v22.0.0
 
 # Automatically determine the distribution
 ARCH=$(uname -m)
@@ -12,9 +11,14 @@ case "$ARCH" in
     *) echo "Unsupported architecture: $ARCH" >&2; exit 1 ;;
 esac
 
-mkdir -p "$NODE_DIR"
-tar -xJvf "node-$VERSION-$DISTRO.tar.xz" -C "$NODE_DIR"
 
-ln -sf /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/node $HOME/bin/node
-ln -sf /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npm $HOME/bin/npm
-ln -sf /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npx $HOME/bin/npx
+
+wget "https://nodejs.org/dist/latest/node-$VERSION-$DISTRO.tar.xz" -O "$NODE_DIR/node-$VERSION-$DISTRO.tar.xz"
+
+mkdir -p "$NODE_DIR"
+tar -xJf "$NODE_DIR/node-$VERSION-$DISTRO.tar.xz" -C "$NODE_DIR"
+rm "$NODE_DIR/node-$VERSION-$DISTRO.tar.xz"
+
+ln -sf "$NODE_DIR/node-$VERSION-$DISTRO/bin/node" "$HOME/bin/node"
+ln -sf "$NODE_DIR/node-$VERSION-$DISTRO/bin/npm" "$HOME/bin/npm"
+ln -sf "$NODE_DIR/node-$VERSION-$DISTRO/bin/npx" "$HOME/bin/npx"
