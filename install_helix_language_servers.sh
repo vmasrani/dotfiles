729eb2e02ba159db89caeb7cf9a229f4c5c384bc
Vaden Masrani <vadmas@gmail.com>
2024-05-08 18:38:15 -0400

    tweaks

--- install_helix_language_servers.sh
+++ install_helix_language_servers.sh
@@ -25,8 +25,9 @@ install_packages() {
 
     check_and_install_packages "ruff-lsp" "ruff-lsp"
     check_and_install_packages "pylsp" "pylsp-mypy"
-    check_and_install_packages "pylsp" "-U 'python-lsp-server[all]'"
-    check_and_install_packages "shellcheck" "-U 'python-lsp-server[all]'"
+    check_and_install_packages "pylsp" "-U python-lsp-server[all]"
+    check_and_install_packages "shellcheck" "shellcheck"
+    
     
     # Deactivate the environment
     conda deactivate
@@ -38,10 +39,12 @@ install_packages default_python_env
 # Install packages in the ml3 environment
 install_packages ml3_env
 
-npm i -g yaml-language-server@next
-npm i -g vscode-langservers-extracted
-npm i -g bash-language-server
-npm i -g pyright
+
+npm list -g yaml-language-server@next || npm i -g yaml-language-server@next
+npm list -g vscode-langservers-extracted || npm i -g vscode-langservers-extracted
+npm list -g bash-language-server || npm i -g bash-language-server
+npm list -g pyright || npm i -g pyright
+
 cargo install --locked --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
 cargo install --git https://github.com/estin/simple-completion-language-server.git
 cargo install taplo-cli --locked --features lsp
