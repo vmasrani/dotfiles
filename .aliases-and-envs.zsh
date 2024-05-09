b477dc1c1ce959c3e9405e25fd255649b5a07aee
Vaden Masrani <vadmas@gmail.com>
2024-05-09 18:10:20 -0400

    tweaks

--- .aliases-and-envs.zsh
+++ .aliases-and-envs.zsh
@@ -77,41 +77,21 @@ alias brew="arch -x86_64 /usr/local/bin/brew"
 alias hxlog="hx /home/vadmas/.cache/helix/helix.log"
 alias reset-tmux='rm -rf ~/.local/share/tmux/resurrect'
 alias zshrc='hx ~/.zshrc'
-alias alsenvs='hx ~/.aliases-and-envs.zsh'
 alias dots='hx ~/dotfiles/'
-alias hxconf='hx ~/dotfiles/hx_config.toml'
+alias rsync='rsync -avz --compress --verbose --human-readable --partial --progress'
+alias ga="lazygit"
+alias bfs='bfs -L '
+alias chals='alias | grep'
 
-# fzf
-
-if [ -d "$HOME/.cursor-server" ]; then
-  export CURSOR_FOLDER=".cursor-server"
-else
-  export CURSOR_FOLDER=".cursor"
-fi
 
 if [ -d "$HOME/.cursor-server/extensions/*tomrijndorp*" ]; then
     export EXTENSION_PATH=$(find ~/.cursor-server/extensions  -type d -name 'tomrijndorp*')
 fi
 
-export GOPROXY=proxy.golang.org
+
 alias ga="lazygit"
 
 # bfs
 alias bfs='bfs -L '
 
-move_and_symlink() {
-    local source="$1"
-    local dest="$2"
 
-    if [ -e "$source" ]; then
-        local source_dir="$(dirname "$source")"
-        local source_name="$(basename "$source")"
-
-        mkdir -p "$dest"
-        mv "$source" "$dest/"
-        ln -si "$dest/$source_name" "$source_dir"
-    else
-        echo "Error: $source does not exist"
-        return 1
-    fi
-}
