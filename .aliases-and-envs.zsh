676beb275ffe9a2632fba3e10c0fa60e9d834e5c
Vaden Masrani <vadmas@gmail.com>
2024-05-10 14:20:28 -0400

    fixed bfs bug

--- .aliases-and-envs.zsh
+++ .aliases-and-envs.zsh
@@ -81,7 +81,7 @@ alias dots='hx ~/dotfiles/'
 alias rsync='rsync -avz --compress --verbose --human-readable --partial --progress'
 alias ga="lazygit"
 alias bfs='bfs -L '
-alias chals='alias | grep'
+alias chals='alias | grep' #check aliases
 
 
 if [ -d "$HOME/.cursor-server/extensions/*tomrijndorp*" ]; then
