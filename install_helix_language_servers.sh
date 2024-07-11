9b2bcd3e247cd8abe1364f96111b878702ac8fa1
Vaden Masrani <vadmas@gmail.com>
2024-07-11 13:56:14 -0400

    fixed bugs in language server script

--- install_helix_language_servers.sh
+++ install_helix_language_servers.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-source ~/miniconda/etc/profile.d/conda.sh
+# Function to check and install packages
 check_and_install_packages() {
-     if ! command -v $1 &> /dev/null
-     then
-         echo "$1 is not installed"
-         eval $2
-     fi
- }
+    if ! command -v $1 &> /dev/null
+    then
+        echo "$1 is not installed"
+        eval $2
+    fi
+}
 
+# Function to install packages in a given environment
+install_packages() {
+    echo "Installing packages in $1 environment"
+    # Activate the environment
+    conda activate $1
+    # Install packages
+    pip install ruff-lsp pylsp-mypy python-lsp-server[all]
+    conda install -c conda-forge shellcheck -y
+    # Deactivate the environment
+    conda deactivate
+}
 
- # Function to install packages in a given environment
- install_packages() {
-     # Activate the environment
-     conda activate $1
-     # Install packages
-
-     check_and_install_packages "ruff-lsp" "pip install ruff-lsp"
-     check_and_install_packages "pylsp" "pip install pylsp-mypy"
-     check_and_install_packages "pylsp" "pip install -U python-lsp-server[all]"
-     check_and_install_packages "shellcheck" "conda install -c conda-forge shellcheck"
-
-     # Deactivate the environment
-     conda deactivate
- }
-
-
-# Install packages in the default Python environment
+# Install conda packages
+echo "Installing conda packages..."
+source ~/miniconda/etc/profile.d/conda.sh
 install_packages base
-
-# Install packages in the ml3 environment
 install_packages ml3
+install_packages shearllama
 
+# Install npm packages
+echo "Installing npm packages..."
+npm install -g yaml-language-server vscode-langservers-extracted bash-language-server pyright
 
-
-sudo -E npm install -g yaml-language-server
-sudo -E npm install -g vscode-langservers-extracted
-sudo -E npm install -g bash-language-server
-sudo -E npm install -g pyright
-
+# Install cargo packages
+echo "Installing cargo packages..."
 cargo install --locked --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
 cargo install --git https://github.com/estin/simple-completion-language-server.git
 cargo install taplo-cli --locked --features lsp
+
+# Helix grammar operations
+echo "Performing Helix grammar operations..."
 hx --grammar build
 hx --grammar fetch
+
+echo "Installation complete!"
