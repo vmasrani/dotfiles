43725de1331ce75b294d1f3383212ca1e437a037
Vaden Masrani <vadmas@gmail.com>
2024-10-23 21:04:16 -0700

    tweaks

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -3,6 +3,8 @@ set -e
 
 apt update && apt upgrade -y
 apt install -y zsh build-essential vim libjpeg-dev zlib1g-dev
+apt install locales
+locale-gen en_US.UTF-8
 chsh -s $(which zsh)
 
 my_dirs=(
@@ -13,7 +15,6 @@ my_dirs=(
     .conda
     .config
     .fzf
-    .gotty.auth
     .ipython
     .jupyter
     .launchpadlib
@@ -40,4 +41,14 @@ for dir in "${my_dirs[@]}"; do
 	echo "Linked $(basename "$source") to $target"
 done
 
-./setup.sh
+files=(.aliases-and-envs.zsh .bash_logout .bash_profile .bashrc .fzf-config.zsh .fzf.bash .fzf.zsh .fzf-env.zsh .gitconfig .p10k.zsh .profile .pylintrc .sourcery.yaml .tmux.conf .vimrc .zlogin .zlogout .zpreztorc .zprofile .zshenv .zshrc .curlrc)
+for file in "${files[@]}"; do
+	echo "Linking $file from dotfiles to home directory."
+	ln -sf "$HOME"/dotfiles/"$file" "$HOME"/"$file"
+done
+
+
+echo "Linking helix from dotfile to ~/.config/helix"
+mkdir -p ~/.config/helix/
+ln -sf ~/dotfiles/hx_config.toml ~/.config/helix/config.toml
+ln -sf ~/dotfiles/hx_languages.toml  ~/.config/helix/languages.toml
