677226c23882f08e824f2f0d280888bc04d589ac
Vaden Masrani <vadmas@gmail.com>
2025-06-18 19:30:33 +0000

    tweaks to npp

--- shell/.aliases-and-envs.zsh
+++ shell/.aliases-and-envs.zsh
@@ -81,7 +81,7 @@ alias ga="lazygit"
 alias bfs='bfs -L'
 alias chals='alias | grep' #check aliases
 alias rename='agent file_renamer'
-alias npp='uv init . && uv add ipython joblib matplotlib numpy pandas pandas_flavor polars pyjanitor requests rich IProgress scikit_learn seaborn torch tqdm pandas numpy requests ipdb PyYAML ipykernel openai ollama git+https://github.com/vmasrani/machine_learning_helpers.git mysql-connector-python --python 3.13 --yes '
+alias npp='uv init . && uv add ipython joblib matplotlib numpy pandas pandas_flavor polars pyjanitor requests rich IProgress scikit_learn seaborn torch tqdm pandas numpy requests ipdb PyYAML ipykernel openai ollama git+https://github.com/vmasrani/machine_learning_helpers.git mysql-connector-python --python 3.10'
 
 alias act='source .venv/bin/activate'
 
